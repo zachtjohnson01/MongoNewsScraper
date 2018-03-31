1bda8908827326c10b6cef3a3d795dfcaa873f20
Zach Johnson <zachtjohnson01@gmail.com>
2018-03-31 08:41:18 -0500

    Corrected scope of res.send(result) in article.js. Was only returning first article from scrape, now returning all articles.

--- routes/article.js
+++ routes/article.js
@@ -5,41 +5,41 @@ var request = require('request');
 var cheerio = require('cheerio');
 
 // middleware that is specific to this router
-router.use(function timeLog (req, res, next) {
-    console.log('Time: ', Date.now())
-    next()
-  });
+// router.use(function timeLog (req, res) {
+//     console.log('Time: ', Date.now())
+//     // next()
+//   });
 
 /* GET home page. */
 router.get('/onion', function(req, res) {
-    console.log('api articles');
+
     request('https://www.theonion.com/', function (error, response, html) {
+        
+        // Load the body of the HTML into cheerio
+        var $ = cheerio.load(html);
+        
+        // Empty array to save our scraped data
+        var results = [];
 
-    // Load the body of the HTML into cheerio
-    var $ = cheerio.load(html);
+        // With cheerio, find each h4-tag with the class "headline-link" and loop through the results
+        $("h1.entry-title").each(function(i, element) {
 
-    // Empty array to save our scraped data
-    var results = [];
+            // console.log(element);
+            // Save the text of the h4-tag as "title"
+            var title = $(element).text();
+            var link = $(element).children().attr('href');
 
-    // With cheerio, find each h4-tag with the class "headline-link" and loop through the results
-    $("h1.entry-title").each(function(i, element) {
-
-        // console.log(element);
-        // Save the text of the h4-tag as "title"
-        var title = $(element).text();
-        var link = $(element).children().attr('href');
-
-        // Make an object with data we scraped for this h4 and push it to the results array
-        results.push({
-            title: title,
-            link: link
-        });
-       
-        res.send(results);
+            // Make an object with data we scraped for this h4 and push it to the results array
+            results.push({
+                title: title,
+                link: link
+            });
+        
             // (i: iterator. element: the current element)
         })
     // res.render('index', { title: 'Mongo Scraper' });
-});
+    res.send(results);
+    });
 });
 
 module.exports = router;
